0f46d750f6bbefdc7c760660985723af522683e7
Bemin <bemin_dawoud@yahoo.com>
2024-02-18 18:26:55 -0800

    update

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,65 +1,36 @@
 #include "sort.h"
-
-/**
- * swap_list - a function to swap two nodes integers
- * @previous: previous node of doubly linked list
- * @temp: node to be swapped
- * @after: next node of doubly linked list
- * @list:  head of list
- */
-void swap_list(listint_t *previous, listint_t *temp, listint_t *after, listint_t **list)
-{
-	while (previous && temp->n < previous->n)
-	{
-		/* handle next node */
-		if (after)
-			after->prev = previous;
-
-		/* handle temp node */
-		temp->next = previous;
-		temp->prev = previous->prev;
-
-		/* handle previous node */
-		if (previous->prev)
-			(previous->prev)->next = temp;
-		previous->prev = temp;
-		previous->next = after;
-
-		/* reset comparison values */
-		after = previous;
-		previous = temp->prev;
-		if ((*list)->prev)
-			*list = (*list)->prev;
-		print_list(*list);
-	}
-}
-
 /**
  * insertion_sort_list - sorts a list with insertion sort
  * @list: input list to sort
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *temp, *previous, *after;
+	listint_t *node = NULL, *temp = NULL;
 
-	if (!list && !(*list) && !(*list)->next)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	temp = (*list)->next;
-	previous = temp->prev;
-	after = temp->next;
-
-	while (temp)
+	node = *list;
+	node = node->next;
+	while (node)
 	{
-		if (temp->n < previous->n)
+		while (node->prev && node->n < (node->prev)->n)
 		{
-			swap_list(previous, temp, after, list);
+			temp = node;
+			if (node->next)
+				(node->next)->prev = temp->prev;
+			(node->prev)->next = temp->next;
+			node = node->prev;
+			temp->prev = node->prev;
+			temp->next = node;
+			if (node->prev)
+				(node->prev)->next = temp;
+			node->prev = temp;
+			if (temp->prev == NULL)
+				*list = temp;
+			print_list(*list);
+			node = node->prev;
 		}
-		temp = after;
-		if (temp)
-		{
-			after = temp->next;
-			previous = temp->prev;
-		}
+		node = node->next;
 	}
 }
