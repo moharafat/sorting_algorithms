ecfb88b07ea13f675cc48d914ab2b2710fcaa515
Bemin Dawoud <69398996+BeminDawoud@users.noreply.github.com>
2024-02-19 04:00:43 +0200

    Create 2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+#include <stdbool.h>
+#include <stdio.h>
+
+/**
+ * swap - swaps the elements in array
+ * @xp: pointer to the integer in an array
+ * @yp: pointer to the integer in an array
+ * Return: 0
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * selection_sort - Sorts an array with selection sort algorithm.
+ * @array: Array to be sorted.
+ * @size: Size of the array.
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_index;
+
+	if (array == NULL || size == 0)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_index])
+				min_index = j;
+		}
+		if (min_index != i)
+		{
+			swap(&array[i], &array[min_index]);
+			print_array(array, size);
+		}
+	}
+}
